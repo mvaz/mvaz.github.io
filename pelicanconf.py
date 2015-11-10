77fc7a2f24586541b32f8be8c5cfad0557699c11
Miguel Vaz <migueljvaz@gmail.com>
2015-11-11 00:24:40 +0100

    changed theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,7 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Specify name of a built-in theme
+THEME = "medius"
+
